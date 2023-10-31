c9f44c3d19827c9d3a64e6fe480d5c42b0ab9546
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-11-01 00:34:17 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express()
+
+
+
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Buy Estate surver is  running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
